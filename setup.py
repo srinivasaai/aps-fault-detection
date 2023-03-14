01ffba03c5ef17e7747e9eda2b3e6a5224599d4f
srinivas <srinivasaai@gmail.com>
2023-03-15 00:01:18 +0530

    updated requirements.txt

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages,setup
+
+def get_requirements():
+    pass
+
+setup(
+    name="sensor",
+    version="0.0.1",
+    author="srinivas",
+    author_email="srinivasaai@gmail.com",
+    packages = find_packages(),
+    install_requires=get_requirements(),
+)
